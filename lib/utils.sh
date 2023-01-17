82070329c3dfa89a9b8ae940cc91ac02926f2797
Trevor Brown <Stratus3D@users.noreply.github.com>
2023-01-16 19:13:48 -0500

    Add github workflow (#28)
    * Add github workflow
    * Add shellcheck and shfmt to workflow
    * Address shellcheck warnings
    * Run shfmt on all files
    * Add .editorconfig file

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,18 +1,25 @@
+#!/usr/bin/env bash
+
 download_source() {
-    local install_type="$1"
-    local version="$2"
-    local download_path="$3"
-    local download_url="$(get_download_url "$install_type" "$version")"
+  local install_type="$1"
+  local version="$2"
+  local download_path="$3"
+  local download_url
 
-    curl -Lo "$download_path" -C - "$download_url"
+  install_type="$1"
+  version="$2"
+  download_path="$3"
+  download_url="$(get_download_url "$install_type" "$version")"
+
+  curl -Lo "$download_path" -C - "$download_url"
 }
-
 
 get_download_url() {
   local install_type="$1"
   local version="$2"
+  local lua_type
 
-  local lua_type="$(get_lua_type "$version")"
+  lua_type="$(get_lua_type "$version")"
 
   if [ "${lua_type}" = "Lua" ]; then
     echo "https://www.lua.org/ftp/lua-${version}.tar.gz"
@@ -22,46 +29,48 @@ get_download_url() {
 }
 
 get_lua_type() {
-    IFS='-' read -a version_info <<< "$1"
-    if [ "${version_info[0]}" = "LuaJIT" ]; then
-        echo "LuaJIT"
-    else
-        echo "Lua"
-    fi
+  IFS='-' read -ra version_info <<<"$1"
+  if [ "${version_info[0]}" = "LuaJIT" ]; then
+    echo "LuaJIT"
+  else
+    echo "Lua"
+  fi
 }
 
 get_lua_version() {
-    IFS='-' read -a version_info <<< "$1"
+  IFS='-' read -ra version_info <<<"$1"
 
-    if [ "${version_info[0]}" = "LuaJIT" ]; then
-        # TODO LuaJIT
-        echo "${version_info[1]}-${version_info[2]}"
+  if [ "${version_info[0]}" = "LuaJIT" ]; then
+    # TODO LuaJIT
+    echo "${version_info[1]}-${version_info[2]}"
+  else
+    # Lua
+    if [ "${#version_info[@]}" -eq 1 ]; then
+      echo "${version_info[0]}"
     else
-        # Lua
-        if [ "${#version_info[@]}" -eq 1 ]; then
-            echo "${version_info[0]}"
-        else
-            echo "${version_info[0]}-${version_info[1]}"
-        fi
+      echo "${version_info[0]}-${version_info[1]}"
     fi
+  fi
 }
 
 get_download_file_path() {
-    local install_type="$1"
-    local version="$2"
-    local tmp_download_dir="$3"
+  local install_type="$1"
+  local version="$2"
+  local tmp_download_dir="$3"
+  local lua_type
+  local lua_version
 
-    local lua_type="$(get_lua_type "$version")"
-    local lua_version="$(get_lua_version "$version")"
+  lua_type="$(get_lua_type "$version")"
+  lua_version="$(get_lua_version "$version")"
 
-    if [ "${lua_type}" = "Lua" ]; then
-        local pkg_name="lua-${lua_version}.tar.gz"
-    fi
+  if [ "${lua_type}" = "Lua" ]; then
+    local pkg_name="lua-${lua_version}.tar.gz"
+  fi
 
-    echo "$tmp_download_dir/$pkg_name"
+  echo "$tmp_download_dir/$pkg_name"
 }
 
 get_latest_luarocks_version() {
-    curl -sL "https://api.github.com/repos/luarocks/luarocks/tags?per_page=1&page=1" |
-        grep '"name"' | cut -d\" -f4 | cut -c2-
+  curl -sL "https://api.github.com/repos/luarocks/luarocks/tags?per_page=1&page=1" |
+    grep '"name"' | cut -d\" -f4 | cut -c2-
 }
